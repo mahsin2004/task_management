6a4ae46acde53112177bfa1881a9d1dec9764788
mahsin <mahsin1912@gmail.com>
2024-04-05 23:51:00 +0600

    Login and register page added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import DashboardHeader from "../components/dashboardHeader";
+import DashboardHeader from "../../components/dashboardHeader";
 import { IoIosAdd, IoMdNotificationsOutline } from "react-icons/io";
 import { Button } from "@/components/ui/button";
 import { MdDashboard } from "react-icons/md";
@@ -18,13 +18,16 @@ export default function DashboardLayout({
         <div className=" w-[95px] flex justify-center items-center">
           <DashboardHeader />
         </div>
+
         <div className="p-[22px] flex gap-4 w-full justify-end border-b-[1.5px] border-[#EAEEF4]">
           <Button className="h-12 rounded-full">
             Add Members <IoIosAdd className="text-[23px] ml-1" />
           </Button>
+
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white">
             <IoMdNotificationsOutline className="text-2xl text-[#7E92A2]" />
           </div>
+
           <Avatar className="w-12 h-12 ">
             <AvatarImage src="https://i.postimg.cc/XYQrcrrr/mahsinmia1.jpg" />
             <AvatarFallback>M</AvatarFallback>
@@ -37,15 +40,19 @@ export default function DashboardLayout({
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white hover:bg-[#514EF3]">
             <MdDashboard className="text-2xl text-[#7E92A2] hover:text-white" />
           </div>
+
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white hover:bg-[#514EF3]">
             <BsFillPeopleFill className="text-2xl text-[#7E92A2] hover:text-white" />
           </div>
+
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white hover:bg-[#514EF3]">
             <FaListCheck className="text-2xl text-[#7E92A2] hover:text-white" />
           </div>
+
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white hover:bg-[#514EF3]">
             <IoCalendarNumber className="text-2xl text-[#7E92A2] hover:text-white" />
           </div>
+          
           <div className="w-12 h-12 rounded-full border-[#EAEEF4] border-[1.5px] flex items-center justify-center bg-white hover:bg-[#514EF3]">
             <IoSettingsSharp className="text-2xl text-[#7E92A2] hover:text-white" />
           </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,12 +2,26 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function Home() {
+export default function register() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="grid gap-3 bg-[#ffff] p-10  rounded-lg border-[1.5px] max-w-sm">
-        <h2 className="text-3xl font-medium text-center mb-3">Login</h2>
-        
+        <h2 className="text-3xl font-medium text-center mb-3">Register</h2>
+
+        <div className="grid grid-cols-2 gap-2">
+          <div className="grid items-center gap-2">
+            <Label htmlFor="name" className="">
+              Full Name
+            </Label>
+            <Input id="name" placeholder="name" className="" />
+          </div>
+
+          <div className="grid items-center gap-2">
+            <Label htmlFor="picture">Photo</Label>
+            <Input id="picture" type="file" className="cursor-pointer" />
+          </div>
+        </div>
+
         <div className="grid items-center gap-2">
           <Label htmlFor="name" className="">
             Email
@@ -22,7 +36,7 @@ export default function Home() {
           <Input id="name" placeholder="password" className="" />
         </div>
 
-        <Button>Login</Button>
+        <Button>Create</Button>
       </div>
     </main>
   );
